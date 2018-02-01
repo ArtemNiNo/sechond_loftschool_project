b8558d5d03a8547eb3bb58efb51abce3780ad150
Artem Lyulin <SeanJoun@gmail.com>
2018-02-01 04:45:44 +0300

    01.02.2018 sechond home work

---
+++ fonts/js/accord.js
@@ -0,0 +1,18 @@
+const comandItem = document.getElementsByClassName('comand__item'),
+      numComandItem = comandItem.length,
+      menuContent = document.getElementsByClassName('menu__content'),
+      numMenuContent = menuContent.length;
+ function ac (a, b, c) {
+    for (var i = 0; i < b; i++) {
+        a[i].addEventListener('click', function() {
+            if(!(this.classList.contains(c))) {
+                for(var i = 0; i < b; i++) {
+                    a[i].classList.remove(c);
+                }
+            this.classList.add(c); 
+            }
+        })
+    }
+}
+ac(menuContent, numMenuContent, 'menu__content--active');
+ac(comandItem, numComandItem, 'comand__item--active');
